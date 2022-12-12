b9a64557eeb1f085f3de6416fc231911c2532d3a
jack <jack68@naver.com>
2022-12-12 18:08:38 +0900

    first my mvc

---
+++ src/test/java/mvc/jkmvc/JkmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.jkmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
